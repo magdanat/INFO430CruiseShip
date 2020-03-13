167390bbc2235cbb31a780191c93439e1c96ce7c
magdanat <nathanmagdalera@gmail.com>
2020-03-12 22:42:08 -0700

    Finish business rules

--- CruiseShip_BR.sql
+++ CruiseShip_BR.sql
@@ -1,5 +1,7 @@
+Use Cruise Go
 -- Business Rules
 -- No more than the capacity for each ship
+-- NO LONGER WORKS WITH CURRENT ERD
 Create Function fNoMoreCapacity()
 Returns Int
 As
@@ -46,3 +48,72 @@ Alter Table tblCUST_BOOK_ACT_TRIP
 Add Constraint CK_NoMoreActivityCapacity
 Check (dbo.fNoMoreActivityCapacity() = 0)
 Go
+
+Select * from tblCountry
+Select * from tblACTIVITY_TYPE
+Select * from tblVENUE_TYPE
+Go
+
+-- Business rule 
+-- No free entertainment and shopping activities at certain venues
+-- for trips that are in the USA
+Create Function fNoFreeEntertainmentAndShoppingActivitiesAtVenues()
+Returns Int
+As 
+Begin
+	Declare @Ret Int = 0
+		If Exists (Select *
+			From tblActivity_Type ACT
+				Join tblActivity A On ACT.ActivityTypeID = A.ActivityTypeID
+				Join tblVenues V On A.VenueID = V.VenueID
+				Join tblVenue_Type VT On V.VenueTypeID = VT.VenueTypeID
+				Join tblCruiseship CS On V.CruiseshipID = CS.CruiseshipID
+				Join tblRoute R On CS.RouteID = R.RouteID
+				Join tblRoute_Location RL On R.RouteID = RL.RouteID
+				Join tblLocation L On RL.LocationID = L.LocationID
+				Join tblCountry C On L.CountryID = C.CountryID
+			Where C.CountryName Like '%USA%'
+				And ACT.ActivityTypeName Like '%Entertainment%'
+				And ACT.ActivityTypeName Like '%Shopping%'
+				And VT.VenueTypeName Like '%Theatre%'
+				And VT.VenueTypeName Like '%Gift Shop%'
+				And A.Cost > 0)
+			Begin Set @Ret = 1
+			End
+			Return @Ret
+		End
+	Go
+
+Alter Table tblActivity
+Add Constraint CK_NoActivityLessThanZeroCost
+Check (dbo.fNoFreeEntertainmentAndShoppingActivitiesAtVenues() = 0)
+Go
+
+-- Business Rule
+-- No activities can be made longer than 4 hours for activities that are food and dining or celebrations
+Create Function fNoActivitiesLongerThan4Hours()
+Returns Int
+As
+Begin
+	Declare @Ret Int = 0
+		If Exists (Select *
+			From tblActivity A
+				Join tblActivity_Trip ACT On A.ActivityID = ACT.ActivityID
+				Join tblActivity_Type ACTT On A.ActivityTypeID = ACTT.ActivityTypeID
+				Join tblVenues V On A.VenueID = V.VenueID
+				Join tblVenue_Type VT On V.VenueTypeID = VT.VenueTypeID
+			Where ACT.StartTime < (Select DateAdd(Hour, -4, ACT.EndTime))
+				And ACTT.ActivityTypeName Like '%Food & Dining%'
+				And ACTT.ActivityTypeName Like '%Celebration%'
+				And VT.VenueTypeName Like '%Deck%')
+			Begin Set @Ret = 1
+			End
+			Return @Ret
+		End
+	Go
+
+Alter Table tblActivity
+Add Constraint CK_fNoActivitiesLongerTha4Hours
+Check (dbo.fNoActivitiesLongerThan4Hours() = 0)
+Go
+
